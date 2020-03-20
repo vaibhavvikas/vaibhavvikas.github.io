3c9d57b9ddb183d16fd757ea3b812ccad79f82cb
Vaibhav Vikas <vbhvvikas@gmail.com>
2020-03-20 16:09:17 +0530

    Added Footer

--- js/cursor.js
+++ js/cursor.js
@@ -1,5 +1,4 @@
 const cursor = document.querySelector('.cursor');
-document.body.style.cursor = 'none';
 document.addEventListener('mousemove', e => {
     cursor.setAttribute("style", "top: "+(e.pageY - 10)+"px; left: "+(e.pageX - 10)+"px;")
 })
